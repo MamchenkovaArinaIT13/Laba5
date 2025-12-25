9d7aaa17cd92db966b41b04573c2c65c142c3a5e
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-12-25 07:10:00 +0300

    Create MeowCounter.java

---
+++ ru.mamchenkova/cats/MeowCounter.java
@@ -0,0 +1,29 @@
+package ru.mamchekova.cats;
+
+public class MeowCounter implements Meowable {
+  private final Cat cat;
+  private int meowCount = 0;
+
+  public MeowCounter() {
+    this.cat = null;
+  }
+
+  public MeowCounter(Cat cat) {
+    this.cat = cat;
+  }
+
+  @Override
+  public void meow() {
+    cat.meow();
+    meowCount++;
+  }
+
+  public int getMeowCount() {
+    return meowCount;
+  }
+
+  @Override
+  public String toString() {
+    return "Вы создани счетчик для мяуканий!";
+  }
+}
