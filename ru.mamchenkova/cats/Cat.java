678ab1007f5575e80afc4c90a9459a147d380946
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-12-25 07:08:52 +0300

    Create Cat.java

---
+++ ru.mamchenkova/cats/Cat.java
@@ -0,0 +1,34 @@
+package ru.mamchekova.cats;
+
+public class Cat implements Meowable {
+  private String name;
+  
+  public Cat() {
+    this.name = null;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Cat(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "кот: " + this.name;
+  }
+
+  @Override
+  public void meow() {
+    System.out.println(this.name + ": мяу!");
+  }
+
+
+
+}
