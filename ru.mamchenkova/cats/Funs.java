43e4fadeb78b16c786fd55a563dd30bdae3438ef
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-12-25 07:09:16 +0300

    Create Funs.java

---
+++ ru.mamchenkova/cats/Funs.java
@@ -0,0 +1,10 @@
+package ru.mamchekova.cats;
+public class Funs {
+  public static void meowsCare(Meowable meowable) {
+    meowable.meow();
+    meowable.meow();
+    meowable.meow();
+    meowable.meow();
+    meowable.meow();
+  }
+}
